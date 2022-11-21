0d1a0cc9b4a800ce58c7c8c2ba0de1c6111a5903
Ksena <ksena@MacBook-Pro-Ks.local>
2022-11-21 17:11:33 +0200

    fix portt

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -52,7 +52,7 @@ export const ProductList = () => {
             queryId,
         }
 
-        fetch('http://bot-telegram-server.vercel.app:3002/web-data', {
+        fetch('http://bot-telegram-server.vercel.app/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
